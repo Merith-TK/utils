f8687082c3e03bd80120d60f73c76e1d6810ca01
Merith-TK <merith.tk@gmail.com>
2022-08-28 18:36:48 -0700

    add Title to debugPrint

--- utils/debugPrint.go
+++ utils/debugPrint.go
@@ -2,11 +2,15 @@ package utils
 
 import "log"
 
-var DebugMode bool = false
+var (
+	DebugMode  bool   = false
+	DebugTitle string = "DEBUG"
+)
 
 // DebugPrint prints the given message to the log if the debug flag is set.
 func DebugPrint(message ...any) {
 	if DebugMode {
-		log.Println(message...)
+		// add DebugTitle to the message
+		log.Println(DebugTitle, message)
 	}
 }
