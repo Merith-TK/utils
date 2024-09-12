95154a703563eb0c55c75deb9252d389f67e58fe
Merith-TK <merith.tk@gmail.com>
2024-09-11 21:09:58 -0700

    add doh-poke

---
+++ cmd/doh-poke/main.go
@@ -0,0 +1,105 @@
+package main
+
+import (
+	"encoding/json"
+	"flag"
+	"fmt"
+	"io/ioutil"
+	"log"
+	"net/http"
+	"net/url"
+	"strings"
+
+	"github.com/Merith-TK/utils/debug"
+)
+
+type DoHResponse struct {
+	Status   int  `json:"Status"`
+	Tc       bool `json:"TC"`
+	Rd       bool `json:"RD"`
+	Ra       bool `json:"RA"`
+	Ad       bool `json:"AD"`
+	Cd       bool `json:"CD"`
+	Question []struct {
+		Name string `json:"name"`
+		Type int    `json:"type"`
+	} `json:"Question"`
+	Answer []struct {
+		Name string `json:"name"`
+		Type int    `json:"type"`
+		TTL  int    `json:"TTL"`
+		Data string `json:"data"`
+	} `json:"Answer"`
+}
+
+func main() {
+	flag.Parse()
+
+	// Parse command line arguments
+	dohServer := flag.Arg(0)
+	domain := flag.Arg(1)
+
+	debug.Print("dohServer:", dohServer)
+	debug.Print("domain:", domain)
+	if !strings.HasPrefix(dohServer, "http://") && !strings.HasPrefix(dohServer, "https://") {
+		dohServer = "https://" + dohServer
+		debug.Print("apply default scheme:", dohServer)
+	}
+
+	urlp, err := url.Parse(dohServer)
+	if err != nil {
+		log.Fatalln("Error parsing URL:", err)
+	}
+	debug.Print("urlp:", urlp)
+
+	urlRebuild := []string{}
+	if urlp.Scheme == "" {
+		urlRebuild = append(urlRebuild, "https://")
+	} else {
+		urlRebuild = append(urlRebuild, urlp.Scheme+"://")
+	}
+	urlRebuild = append(urlRebuild, urlp.Host)
+	if urlp.Path == "" || urlp.Path == "/" || urlp.Path == flag.Arg(0) {
+		urlRebuild = append(urlRebuild, "/dns-query")
+	} else {
+		urlRebuild = append(urlRebuild, urlp.Path)
+	}
+	st := strings.Join(urlRebuild, "")
+	debug.Print("st:", st)
+
+	// Create the query URL
+	queryURL := fmt.Sprintf("%s?name=%s&type=A", st, domain)
+	req, err := http.NewRequest("GET", queryURL, nil)
+	if err != nil {
+		log.Fatalf("Failed to create HTTP request: %v", err)
+	}
+	req.Header.Set("Accept", "application/dns-json")
+	resp, err := http.DefaultClient.Do(req)
+	if err != nil {
+		log.Fatalf("Failed to perform DNS query: %v", err)
+	}
+	defer resp.Body.Close()
+	if err != nil {
+		log.Fatalf("Failed to perform DNS query: %v", err)
+	}
+	defer resp.Body.Close()
+
+	// Read the response body
+	body, err := ioutil.ReadAll(resp.Body)
+	if err != nil {
+		log.Fatalf("Failed to read response body: %v", err)
+	}
+
+	// Parse the response body into a DoHResponse struct
+	var dohResponse DoHResponse
+	err = json.Unmarshal(body, &dohResponse)
+	if err != nil {
+		log.Println("Failed to parse JSON response:", err)
+		log.Println("Response body:", string(body))
+	}
+
+	// Print the answer
+	for _, answer := range dohResponse.Answer {
+		fmt.Println("Answer:", answer.Data)
+	}
+}
--- go.mod
+++ go.mod
@@ -10,13 +10,21 @@ require (
 	github.com/gen2brain/beeep v0.0.0-20240516210008-9c006672e7f4
 	github.com/gorilla/websocket v1.5.3
 	github.com/lucasb-eyer/go-colorful v1.2.0
+	github.com/miekg/dns v1.1.62
 )
 
 require (
 	github.com/Tnze/go-mc v1.20.1-pre1 // indirect
 	github.com/go-toast/toast v0.0.0-20190211030409-01e6764cf0a4 // indirect
 	github.com/godbus/dbus/v5 v5.1.0 // indirect
+	github.com/jamescun/doh v0.0.0-20180806162537-183f0ccfacf7 // indirect
+	github.com/josharian/intern v1.0.0 // indirect
+	github.com/mailru/easyjson v0.7.7 // indirect
 	github.com/nu7hatch/gouuid v0.0.0-20131221200532-179d4d0c4d8d // indirect
 	github.com/tadvi/systray v0.0.0-20190226123456-11a2b8fa57af // indirect
+	golang.org/x/mod v0.18.0 // indirect
+	golang.org/x/net v0.27.0 // indirect
+	golang.org/x/sync v0.7.0 // indirect
 	golang.org/x/sys v0.25.0 // indirect
+	golang.org/x/tools v0.22.0 // indirect
 )
